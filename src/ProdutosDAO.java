017952cc5ccd345c7f0cbab1adf7d07586b643e5
Markko1982 <umbrasileironarussia@gmail.com>
2026-03-14 13:15:36 -0300

    Implementa listagem de produtos cadastrados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -46,10 +46,35 @@ public boolean cadastrarProduto(ProdutosDTO produto){
     }
 }
     
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
-        return listagem;
+  public ArrayList<ProdutosDTO> listarProdutos(){
+
+    String sql = "SELECT * FROM produtos";
+    conn = new conectaDAO().connectDB();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        resultset = prep.executeQuery();
+
+        while (resultset.next()) {
+            ProdutosDTO produto = new ProdutosDTO();
+
+            produto.setId(resultset.getInt("id"));
+            produto.setNome(resultset.getString("nome"));
+            produto.setValor(resultset.getInt("valor"));
+            produto.setStatus(resultset.getString("status"));
+
+            listagem.add(produto);
+        }
+
+        prep.close();
+        conn.close();
+
+    } catch (Exception erro) {
+        JOptionPane.showMessageDialog(null, "Erro ao listar produtos: " + erro.getMessage());
     }
+
+    return listagem;
+}
     
     
     
